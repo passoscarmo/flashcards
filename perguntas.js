a5a93e8d9c3444e0f2c0b14b9c7463826982f47c
passoscarmo <00001081076781sp@al.educacao.sp.gov.br>
2024-11-20 15:30:50 -0300

    adicionando arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Escola',
+    'Quando acabam as aulas?',
+    'Em duas semanas'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior produtor de cerveja do mundo?',
+    'Segundo o Sindicato Nacional da Indústria da Cerveja, o Sindicerv, o Brasil mantém a terceira posição no ranking dos maiores produtores de cerveja no mundo, atrás apenas da China e dos Estados Unidos'
+)
+
+criaCartao(
+    'Programação',
+    'Quem é o professor de tecnologia?',
+    'O careca do Márcio'
+)
+
+criaCartao(
+    'Aulas',
+    'Qual a melhor aula pra dormir?',
+    'Todas'
+)
